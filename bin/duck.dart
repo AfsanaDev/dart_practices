bb31aa0f778c697a9cc82dc751468dd35ce1138c
Afsana Akter <tarinakter567@gmail.com>
2023-09-12 11:34:05 +0600

    main function initialize.

--- bin/duck.dart
+++ bin/duck.dart
@@ -1,37 +1,58 @@
 import 'dart:io';
 class Duck{
   void quack(){
-    stdout.write("Duck: All types of duck can fly");
+    print("Duck: All types of duck can fly");
   }
   void swim(){
-    stdout.write("Duck: all duck can swim");
+    print("Duck: all duck can swim");
   }
   void fly(){
-    stdout.write("Duck: all duck can fly");
+    print("Duck: all duck can fly");
   }
   void display(){
-    stdout.write("Duck: all duck can display");
+    print("Duck: all duck can display");
   }
 }
 
 class MallarDuck extends Duck{
   @override
   void fly(){
-    stdout.write("MallarDuck: Mallar duck can fly");
+    print("MallarDuck: Mallar duck can fly");
   }
   @override
   void display(){
-    stdout.write("MallarDuck: Mallar duck can display");
+    print("MallarDuck: Mallar duck can display");
   }
 }
 
 class RedHeadDuck extends Duck{
   @override
   void fly(){
-    stdout.write("Red Head Duck : Red head duck can fly");
+    print("Red Head Duck : Red head duck can fly");
   }
   @override
   void display(){
-    stdout.write("Red Head Duck: Red head duck can display");
+    print("Red Head Duck: Red head duck can display");
   }
+}
+
+void main(){
+  Duck duck = Duck();
+  duck.quack();
+  duck.swim();
+  duck.display();
+  duck.fly();
+
+
+  MallarDuck mallarDuck = MallarDuck();
+  mallarDuck.quack();
+  mallarDuck.swim();
+  mallarDuck.display();
+  mallarDuck.fly();
+
+  RedHeadDuck redHeadDuck = RedHeadDuck();
+  redHeadDuck.quack();
+  redHeadDuck.swim();
+  redHeadDuck.display();
+  redHeadDuck.fly();
 }
